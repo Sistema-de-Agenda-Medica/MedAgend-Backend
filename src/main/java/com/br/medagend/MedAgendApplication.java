1b248588fe97896868971699be91a848f65a4891
Hebert Soares <hebertsoaresof@gmail.com>
2024-12-16 00:56:20 -0300

    Estrutura do projeto

---
+++ src/main/java/com/br/medagend/MedAgendApplication.java
@@ -0,0 +1,13 @@
+package com.br.medagend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAgendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedAgendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/medagend/MedAgendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.medagend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAgendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
